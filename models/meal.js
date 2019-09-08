ddba61131186beb767a54eecb42b7b55c00a2a86
chris wang <wangguanzzz@gmail.com>
2019-09-08 13:28:10 +0800

    update

---
+++ models/meal.js
@@ -0,0 +1,31 @@
+class Meal {
+  constructor(
+    id,
+    categoryIds,
+    title,
+    afforability,
+    imageUrl,
+    duration,
+    ingredients,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactoseFree
+  ) {
+    this.id = id;
+    this.categoryIds = categoryIds;
+    this.title = title;
+    this.afforability = afforability;
+    this.imageUrl = imageUrl;
+    this.duration = duration;
+    this.ingredients = ingredients;
+    this.steps = steps;
+    this.isGlutenFree = isGlutenFree;
+    this.isVegan = isVegan;
+    this.isVegetarian = isVegetarian;
+    this.isLactoseFree = isLactoseFree;
+  }
+}
+
+export default Meal;
